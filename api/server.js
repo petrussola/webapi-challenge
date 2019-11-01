4df72234fb9cf88bd28631d4c72a6d4ccd13f9da
Pere <pere.solaclaver@gmail.com>
2019-11-01 09:02:55 +0000

    GET endpoint for projects

--- api/server.js
+++ api/server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 require("dotenv").config();
+const Projects = require("../data/helpers/projectModel");
+const Actions = require("../data/helpers/actionModel");
 
 // INITIALIZE EXPRESS INSTANCE
 
@@ -17,9 +19,23 @@ server.use(express.json());
 
 // ENDPOINTS
 
+server.get("/projects", (req, res) => {
+  Projects.get()
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .error(error => {
+      res
+        .status(500)
+        .json({ message: `There was an error retrieving projects: ${error}` });
+    });
+});
+
 server.get("/", (req, res) => {
   res.status(200).json({ message: "hello from server" });
 });
+
+// MIDDLEWARE
 
 // EXPORT
 
