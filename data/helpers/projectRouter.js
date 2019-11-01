2c91ac96495b28b97d8aa394898a5ae62dbd0cfb
Pere <pere.solaclaver@gmail.com>
2019-11-01 10:38:25 +0000

    POST actions inside project endpoint working with project id validation and erro fallback

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -1,6 +1,7 @@
 // IMPORTS
 const express = require("express");
 const Projects = require("./projectModel");
+const Actions = require("./actionModel");
 
 // INSTANTIATE EXPRESS
 const router = express.Router();
@@ -39,6 +40,8 @@ router.get("/:id/actions", validateProjectId, (req, res) => {
 
 // POST
 
+// POST PROJECT
+
 router.post("/", validateProject, (req, res) => {
   Projects.insert(req.body)
     .then(data => {
@@ -48,6 +51,22 @@ router.post("/", validateProject, (req, res) => {
       res.status(400).json({
         message: `There was an error posting the project: ${error.message}`
       });
+    });
+});
+
+// POST PROJECT ACTIONS
+
+router.post("/:id/actions", validateProjectId, (req, res) => {
+  Actions.insert(req.body)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({
+          message: `Could not post actions into project ${req.data.id}: ${error.message}`
+        });
     });
 });
 
@@ -72,11 +91,9 @@ router.put("/:id", [validateProjectId, validateProject], (req, res) => {
       res.status(200).json(data);
     })
     .catch(error => {
-      res
-        .status(500)
-        .json({
-          message: `Project ${req.data.id} could not be edited: ${error.message}`
-        });
+      res.status(500).json({
+        message: `Project ${req.data.id} could not be edited: ${error.message}`
+      });
     });
 });
 
